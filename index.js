e5a699ef2de02be60c7d49b607195d927110dcbc
Jordy756 <jordycastro1756@gmail.com>
2025-05-02 13:26:02 -0600

    feat: add initial HTML structure and styling for the AluraGeek API documentation

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express, { json } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 import { PORT } from "./src/config/environment.js";
 import { getConnection } from "./src/config/database.js";
 import { createAPIRouter } from "./api.router.js";
 import { errorMiddleware } from "./src/middlewares/errorMiddleware.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 getConnection();
 
@@ -19,12 +24,10 @@ app.use(
 
 app.use(json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/api", createAPIRouter());
-
 app.use(errorMiddleware);
 
-app.get("/", (req, res) => {
-    res.send("Subscribe to Arpan Neupane's channel");
-});
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
 
-app.listen(PORT || 5000, () => console.log(`Running on http://localhost:${PORT || 5000}/api`));
+app.listen(PORT || 5000, () => console.log(`Running on http://localhost:${PORT || 5000}`));
