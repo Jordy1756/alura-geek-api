2c678f1128b220eb71aaa2d133f2f7004cd44439
Jordy756 <jordycastro1756@gmail.com>
2025-05-02 02:26:14 -0600

    refactor: update index.js structure and change server port configuration

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-import express, { json } from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import { PORT } from "./src/config/environment.js";
-import { getConnection } from "./src/config/database.js";
-import { createAPIRouter } from "./api.router.js";
-import { errorMiddleware } from "./src/middlewares/errorMiddleware.js";
+// import express, { json } from "express";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+// import { PORT } from "./src/config/environment.js";
+// import { getConnection } from "./src/config/database.js";
+// import { createAPIRouter } from "./api.router.js";
+// import { errorMiddleware } from "./src/middlewares/errorMiddleware.js";
 
-getConnection();
+// getConnection();
 
+// const app = express();
+// app.disable("x-powered-by");
+// app.use(
+//     cors({
+//         origin: "http://localhost:5501",
+//         credentials: true,
+//     })
+// );
+
+// app.use(json());
+// app.use(cookieParser());
+// app.use("/api", createAPIRouter());
+
+// app.use(errorMiddleware);
+// app.listen(PORT || 5000, () => console.log(`Running on http://localhost:${PORT || 5000}/api`));
+
+const express = require("express");
 const app = express();
-app.disable("x-powered-by");
-app.use(
-    cors({
-        origin: "http://localhost:5501",
-        credentials: true,
-    })
-);
 
-app.use(json());
-app.use(cookieParser());
-app.use("/api", createAPIRouter());
+const port = process.env.PORT || 8080;
 
-app.use(errorMiddleware);
-app.listen(PORT || 5000, () => console.log(`Running on http://localhost:${PORT || 5000}/api`));
+app.get("/", (req, res) => {
+    res.send("Subscribe to Arpan Neupane's channel");
+});
+
+app.listen(port, () => {
+    `Server started on port ${port}`;
+});
